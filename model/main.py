4078828b488362bba74f71c0a5e816cea0e5ce82
janhvisingh14 <janhvisingh14102002@gmail.com>
2025-11-12 13:10:32 +0530

    data path

--- model/main.py
+++ model/main.py
@@ -32,7 +32,7 @@ def create_model(data):
 
 
 def get_clean_data():
-    data = pd.read_csv("d:/Cancer_Prediction/data/data.csv")
+    data = pd.read_csv("data/data.csv")
     data = data.drop(['Unnamed: 32','id'],axis=1)
     data['diagnosis'] = data['diagnosis'].map({'M':1,'B':0})
 
